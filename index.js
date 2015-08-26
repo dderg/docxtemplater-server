9b8e0e80d630f509d7ed107c764a288c8e932713
Danila Dergachev <gnomdan@yandex.ru>
2015-08-26 17:10:27 +0300

    multipart and string with url or json support

--- index.js
+++ index.js
@@ -9,14 +9,27 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var formidable = require('formidable');
 var app = express();
+var isJSON = require('is-json');
+var validUrl = require('valid-url');
+var httpreq = require('httpreq');
 app.use(bodyParser.urlencoded({extended: true}));
 
 var chartModule = new ChartModule();
 var imageModule = new ImageModule();
 
-app.post('/', function (req, res) {
-	// console.log(JSON.parse(req.body.json));
-	
+
+var render = function (content, data) {
+	var doc = new Docxtemplater()
+		.attachModule(chartModule)
+		.attachModule(imageModule)
+		.load(content)
+		.setData(data)
+		.render();
+	return doc.getZip().generate({type: "nodebuffer"});
+}
+
+
+var parseForm = function (req, res) {
 	var form = new formidable.IncomingForm();
 
 	form.parse(req, function(err, fields, files) {
@@ -33,15 +46,65 @@ app.post('/', function (req, res) {
 		}
 		var content = fs.readFileSync(files.docx.path, "binary");
 		var data = JSON.parse(fs.readFileSync(files.json.path, "utf8"));
-		var doc = new Docxtemplater()
-			.attachModule(chartModule)
-			.attachModule(imageModule)
-			.load(content)
-			.setData(data)
-			.render();
-		var buf = doc.getZip().generate({type: "nodebuffer"});
+		var buf = render(content, data);
 		res.end(buf);
-	})
+	});
+};
+
+var download = function (url, cb) {
+	var data = [], dataLen = 0;
+	var request = httpreq.get(url, {binary: true}, function (err, response) {
+		if (err) {
+			res.status(500).end(err);
+			cb(err);
+			return;
+		}
+		cb(undefined, response.body);
+	});
+};
+
+app.post('/', function (req, res) {
+	var contentType = req.headers['content-type'];
+	if (contentType.split('/')[0] === 'multipart') {
+		parseForm(req, res);
+	} else {
+		var json, docx, jsonLoaded = false, docxLoaded = false;
+		var isLoaded = function () {
+			return (jsonLoaded && docxLoaded);
+		}
+		var handleForm = function () {
+			if (isLoaded()) {
+				var buf = render(docx, json);
+				res.end(buf);
+			}
+		}
+		if (isJSON(req.body.json)) {
+			json = JSON.parse(req.body.json);
+			jsonLoaded = true;
+			handleForm();
+		} else if (validUrl.isUri(req.body.json)) {
+			download(req.body.json, function (error, data) {
+				if (error) {
+					res.status(400).end('could not download json with link: ' + req.body.json);
+					return;
+				}
+				json = data;
+				jsonLoaded = true;
+				handleForm();
+			});
+		}
+		if (validUrl.isUri(req.body.docx)) {
+			download(req.body.docx, function (error, data) {
+				if (error) {
+					res.status(400).end('could not download docx with link: ' + req.body.docx);
+					return;
+				}
+				docx = data;
+				docxLoaded = true;
+				handleForm();
+			});
+		}
+	}
 });
 
 
--- test/test.js
+++ test/test.js
@@ -5,8 +5,11 @@ var expect = require('chai').expect;
 describe('http', function () {
     this.timeout(10000)
     it ('should generate docx in response', function (done) {
-        var writeStream = fs.createWriteStream(__dirname + '/../chart.docx');
+        var resultPath = __dirname + '/../chart.docx';
+        var writeStream = fs.createWriteStream(resultPath);
         writeStream.on('close', function () {
+            var file = fs.readFileSync(resultPath);
+            expect(file.length).to.equal(40213);
             done();
         })
         request.post({ 
@@ -15,6 +18,30 @@ describe('http', function () {
                 json: fs.createReadStream(__dirname + '/../example/chartExample.json'),
                 docx: fs.createReadStream(__dirname + '/../example/chartExample.docx')
             }
+        })
+        .on('error', function (error) {
+            throw error;
+        })
+        .on('response', function (response) {
+            expect(response.statusCode).to.equal(200);
+        })
+        .pipe(writeStream);
+    })
+    it ('should get json as text and docx as link', function (done) {
+        var resultPath = __dirname + '/../chartLinked.docx';
+        var writeStream = fs.createWriteStream(resultPath);
+        writeStream.on('close', function () {
+            var file = fs.readFileSync(resultPath);
+            expect(file.length).to.equal(40213);
+            done();
+        })
+        request.post({ 
+            url: 'http://localhost:3000/',
+            form: {
+                docx: 'http://front-end.fmake.ru/docxtemplater-server/example/chartExample.docx',
+                json: fs.readFileSync(__dirname + '/../example/chartExample.json')
+            }
+            
         })
         .on('error', function (error) {
             throw error;
